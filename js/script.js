c22854ae1251876bf4c1c19016e3672944e382af
dimkaar <dimkaar1102@gmail.com>
2017-06-20 18:15:43 +0300

    Script to open-close has been added

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var navMain = document.querySelector('.main-header__nav');
 var navToggle = document.querySelector('.main-header__toggle');
+var buyBtn = document.querySelector('.product-article__btn');
+var modalWindow = document.querySelector('.modal-block');
+var bodyOverlay = document.querySelector('.body-overlay');
 
 navMain.classList.remove('main-header__nav--nojs');
 
@@ -10,5 +13,26 @@ navToggle.addEventListener('click', function(){
   } else {
     navMain.classList.remove('main-header__nav--opened');
     navMain.classList.add('main-header__nav--closed');
+  }
+});
+
+
+buyBtn.addEventListener('click', function() {
+  event.preventDefault();
+  if(modalWindow.style.display = 'none') {
+    modalWindow.style.display = 'flex';
+    bodyOverlay.style.display = 'block';
+  } else  {
+    return 0;
+  }
+});
+
+
+window.addEventListener('keydown', function(event) {
+  if (event.keyCode === 27) {
+    if (modalWindow.style.display = 'flex') {
+      modalWindow.style.display = 'none';
+      bodyOverlay.style.display = 'none';
+    }
   }
 });
